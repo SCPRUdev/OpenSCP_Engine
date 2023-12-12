c273a68d090c35be1a9b7ad5cd2f4d8e3a550bd8
OsobistSCP <osobist@scpfoundation.net>
2023-12-12 10:51:21 +0700

    <RESTORE>Unhardcoded debug messages for customization

--- src/Battlescape/BattlescapeState.cpp
+++ src/Battlescape/BattlescapeState.cpp
@@ -2743,7 +2743,7 @@ inline void BattlescapeState::handle(Action *action)
 						_map->getSelectorPosition(&newPos);
 						if (_save->getTileEngine()->isPositionValidForUnit(newPos, unit))
 						{
-							debug("Beam me up Scotty");
+							debug(tr("STR_DEBUG_TELEPORT_UNIT"));
 							_save->getPathfinding()->removePreview();
 
 							unit->setTile(_save->getTile(newPos), _save);
@@ -2763,12 +2763,12 @@ inline void BattlescapeState::handle(Action *action)
 					if (key == SDLK_d && ctrlPressed)
 					{
 						_save->setDebugMode();
-						debug("Debug Mode");
+						debug(tr("STR_DEBUGMODE"));
 					}
 					// "ctrl-v" - reset tile visibility
 					else if (_save->getDebugMode() && key == SDLK_v && ctrlPressed)
 					{
-						debug("Resetting tile visibility");
+						debug(tr("STR_DEBUG_SHOWMAP"));
 						_save->resetTiles();
 					}
 					else if (_save->getDebugMode() && (key == SDLK_k || key == SDLK_j) && ctrlPressed)
@@ -2790,7 +2790,7 @@ inline void BattlescapeState::handle(Action *action)
 							// kill (ctrl-shift-k) or stun (ctrl-shift-j) just a single unit (under the cursor)
 							if (unitUnderTheCursor && !unitUnderTheCursor->isOut())
 							{
-								debug("Bingo!");
+								debug(tr("STR_DEBUG_STUN_KILL_ONE"));
 								unitUnderTheCursor->damage(Position(0, 0, 0), 1000, _game->getMod()->getDamageType(stunOnly ? DT_STUN : DT_AP), _save, {});
 							}
 						}
@@ -2799,12 +2799,12 @@ inline void BattlescapeState::handle(Action *action)
 							if (stunOnly)
 							{
 								// "ctrl-j" - stun all aliens
-								debug("Deploying Celine Dion album");
+								debug(tr("STR_DEBUG_STUNALL"));
 							}
 							else
 							{
 								// "ctrl-k" - kill all aliens
-								debug("Influenza bacterium dispersed");
+								debug(tr("STR_DEBUG_KILLALL"));
 							}
 							for (auto* bu : *_save->getUnits())
 							{
